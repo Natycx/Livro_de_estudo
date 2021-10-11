fe85e553bcecba5914677a3b9cab6775aaa4f6aa
Natalia <talia_garcia@hotmail.com>
2021-10-10 21:21:14 -0300

    Continuação livro

--- Livro_python.py
+++ Livro_python.py
@@ -386,8 +386,31 @@ def obtencao_de_valor_dicionario():
             print('Produto não encontrado')
 
 
+def dicionario_com_estoque():
+    estoque = {'alface': [500, 0.45],
+               'Batata': [2001, 1.20],
+               'Tomate': [1000, 2.30],
+               'Feijão': [100, 1.50]}
+    venda = [['Tomate', 5], ['Batata', 10], ['alface', 5]]
+    total = 0
+    print('Vendas: \n')
+    for operacao in venda:
+        produto, quantidade = operacao
+        preco = estoque[produto][1]
+        custo = preco * quantidade
+        print(f'{produto:12s} : {quantidade:3d} x {preco:6.2f} = {custo:6.2f}')
+        estoque[produto][0] -= quantidade
+        total += custo
+    print(f'Custo total: {total:21.2f}\n')
+    print('Estoque: \n')
+    for chave, dados in estoque.items():
+        print('Descrição: ', chave)
+        print('Quantidade: ', dados[0])
+        print(f'Preço: {dados[1]:6.2f}\n')
+
+
 def main():
-    obtencao_de_valor_dicionario()
+    dicionario_com_estoque()
 
 
 if __name__ == '__main__':
